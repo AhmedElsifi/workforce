4b0d33edd5b9e65ec83a6b391c4befb1700389b6
Ahmed Elsifi <ahmedelsifi06@gmail.com>
2026-09-25 23:30:36 +0300

    fix: remove trailing slash from CORS origin URL

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://127.0.0.1:5500",
-      "https://iti-project-workforce.vercel.app/",
+      "https://iti-project-workforce.vercel.app",
     ],
     credentials: true,
   }),
